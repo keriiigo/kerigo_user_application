ad40126d8cf4d8207704cd64fa2db9033d8642c7
Muhammed Midlaj <muhammedmidlaj8395@gmail.com>
2024-07-25 14:47:46 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/kerigo_user_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.kerigo_user_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:kerigo_user_app/my_app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
